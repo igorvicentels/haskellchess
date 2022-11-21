e3830a27d8f031394388ac96c070fbf76c1f76f1
igorvicentels <igorvicente97@gmail.com>
2022-11-21 20:01:36 -0300

    testing types to represent pieces and board

---
+++ Board.hs
@@ -0,0 +1,71 @@
+module Board where
+
+data Piece = BPawn
+           | BRook
+           | BKnight
+           | BBishop
+           | BQueen
+           | BKing
+           | WPawn
+           | WRook
+           | WKnight
+           | WBishop
+           | WQueen
+           | WKing
+           | Empty
+    
+instance Show Piece where
+    show BPawn   = "p"
+    show BRook   = "r"
+    show BKnight = "n"
+    show BBishop = "b"
+    show BQueen  = "q"
+    show BKing   = "k"
+    show WPawn   = "P"
+    show WRook   = "R"
+    show WKnight = "N"
+    show WBishop = "B"
+    show WQueen  = "Q"
+    show WKing   = "K"
+    show Empty   = " "
+
+type Board = [[Piece]]
+-- TODO: verify number of rows and columns
+
+printRow :: [Piece] -> IO ()
+printRow []     = return () 
+printRow [x]    = putStrLn $ show x 
+printRow (x:xs) = do putStr $ show x 
+                     putStr " | "
+                     printRow xs
+
+printBoard :: Board -> IO ()
+printBoard []     = return ()
+printBoard [x]    = printRow x
+printBoard (x:xs) = do printRow x
+                       printLine 
+                       printBoard xs
+
+
+printLine :: IO ()
+printLine = putStrLn "------------------------------"
+
+
+
+testRow1 = [BRook, BKnight, BBishop, BQueen, BKing, BBishop, BKnight, BRook]
+testRow2 = [BPawn, BPawn, BPawn, BPawn, BPawn, BPawn, BPawn, BPawn]
+testRow3 = [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+testRow4 = [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+testRow5 = [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+testRow6 = [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+testRow7 = [WPawn, WPawn, WPawn, WPawn, WPawn, WPawn, WPawn, WPawn]
+testRow8 = [WRook, WKnight, WBishop, WQueen, WKing, WBishop, WKnight, WRook]
+
+testBoard = [ testRow1
+            , testRow2
+            , testRow3
+            , testRow4
+            , testRow5
+            , testRow6
+            , testRow7
+            , testRow8 ] 
